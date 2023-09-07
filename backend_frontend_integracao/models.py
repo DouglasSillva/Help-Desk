74533f6c790943e235bcd59fd579e1612883d058
jefersonrodrigal <jefersonrodrigoalmeida@outlook.com>
2023-09-07 08:15:06 -0300

    criacao das rotas para login e logout

--- backend_frontend_integracao/models.py
+++ backend_frontend_integracao/models.py
@@ -1,15 +1,16 @@
 from sqlalchemy import Column, create_engine
-from sqlalchemy import Integer, String, ForeignKey, Boolean
-from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import Integer, String, ForeignKey, Boolean, DateTime
+from sqlalchemy.orm import DeclarativeBase, relationship, Mapped
+from flask_login import UserMixin
 
-engine = create_engine("sqlite:///homolog.db", echo=True)
+engine = create_engine("sqlite:///homolog.db", echo=False)
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class User(Base):
+class User(Base, UserMixin):
     __tablename__ = 'usuarios'
 
     id = Column(Integer, primary_key=True)
@@ -17,6 +18,10 @@ class User(Base):
     lastname = Column(String(length=100), name='sobrenome', nullable=False)
     email = Column(String(250), unique=True, nullable=False)
     password = Column(String(250), nullable=False)
+    token = Column(String(250), nullable=True)
+    last_session = Column(DateTime)
+
+    reports: Mapped['Reports'] = relationship(back_populates='users')
 
     def __repr__(self):
         return f"User(id={self.id!r}, name={self.name!r})"
@@ -35,6 +40,8 @@ class Reports(Base):
 
     user_id = Column(Integer, ForeignKey(column='usuarios.id'),
                      name='usuarios_id', nullable=False)
+
+    users: Mapped['User'] = relationship(back_populates='reports')
 
     def __repr__(self):
         return f"Report(id={self.id!r}, subject={self.subject!r})"
